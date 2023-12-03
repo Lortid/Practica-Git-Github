b6fb7f5b620e883de7dc391cee2ef1f93fca4c9c
Lortid <Jaosoriogo@unal.edu.co>
2023-12-03 12:43:47 -0500

    Se añade algo nuevo

--- Jaime Osorio.py
+++ Jaime Osorio.py
@@ -3,7 +3,7 @@ b = input("Ingresa lo que estudias:")
 
 if a.lower() == "jaime osorio" and b.lower() == "ingeniería de sistemas":
     print("Eres Jaime Osorio")
-    print("Programa finalizado, se ha verificado su identidad")
+    print("Programa finalizado, se ha verificado su identidad, eres Jaime GOD")
 
 else:
     print("No eres Jaime Osorio")
