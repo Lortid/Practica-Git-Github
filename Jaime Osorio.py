53415163a4ad955a9cd45cebabc19be31019fce5
Lortid <Jaosoriogo@unal.edu.co>
2023-12-03 11:41:49 -0500

    Se añadió un código de python básico de strings

---
+++ Jaime Osorio.py
@@ -0,0 +1,8 @@
+a = input("Ingresa tu nombre :")
+b = input("Ingresa lo que estudias:")
+
+if a.lower() == "jaime osorio" and b.lower() == "ingeniería de sistemas":
+    print("Eres Jaime Osorio")
+
+else:
+    print("No eres Jaime Osorio")
