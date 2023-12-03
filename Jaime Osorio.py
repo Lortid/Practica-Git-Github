e75750750c9cffbbb612f7061ea9bdc61e9249f1
Lortid <Jaosoriogo@unal.edu.co>
2023-12-03 12:26:03 -0500

    Se corrigió un conflicto inesperado

--- Jaime Osorio.py
+++ Jaime Osorio.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 a = input("Ingresa tu nombre :")
 b = input("Ingresa lo que estudias:")
 
@@ -6,13 +5,4 @@ if a.lower() == "jaime osorio" and b.lower() == "ingeniería de sistemas":
     print("Eres Jaime Osorio")
 
 else:
-=======
-a = input("Ingresa tu nombre :")
-b = input("Ingresa lo que estudias:")
-
-if a.lower() == "jaime osorio" and b.lower() == "ingeniería de sistemas":
-    print("Eres Jaime Osorio")
-
-else:
->>>>>>> 8e347d09e07ba5f69bc1b152b92398593dcc4a5c
     print("No eres Jaime Osorio")
