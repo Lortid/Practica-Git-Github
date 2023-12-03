2c5728194a2a0c6603559f28691ed9083e8573ac
Jaime Luis Osorio Gómez <123094739+Lortid@users.noreply.github.com>
2023-12-03 12:30:50 -0500

    Nueva línea
    Se agregó una nueva línea de código donde  finaliza el programa.

--- Jaime Osorio.py
+++ Jaime Osorio.py
@@ -3,6 +3,8 @@ b = input("Ingresa lo que estudias:")
 
 if a.lower() == "jaime osorio" and b.lower() == "ingeniería de sistemas":
     print("Eres Jaime Osorio")
+    pritn("Programa finalizado, se ha verificado su identidad"
 
 else:
     print("No eres Jaime Osorio")
+    
